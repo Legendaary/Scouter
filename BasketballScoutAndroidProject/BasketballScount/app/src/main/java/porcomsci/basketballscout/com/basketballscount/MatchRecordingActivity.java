3e41bdb5854a81a8dadb4b0ed0e92363dac42353
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-06-14 19:07:30 +0700

    add MatchRecordingActivity

--- BasketballScoutAndroidProject/BasketballScount/app/src/main/java/porcomsci/basketballscout/com/basketballscount/MatchRecordingActivity.java
+++ BasketballScoutAndroidProject/BasketballScount/app/src/main/java/porcomsci/basketballscout/com/basketballscount/MatchRecordingActivity.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MatchRecord extends ActionBarActivity {
+public class MatchRecordingActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_match_record);
+        setContentView(R.layout.activity_match_recording);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_match_record, menu);
+        getMenuInflater().inflate(R.menu.menu_match_recording, menu);
         return true;
     }
 
