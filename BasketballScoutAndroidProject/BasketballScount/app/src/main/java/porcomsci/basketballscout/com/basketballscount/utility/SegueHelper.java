e6e85a9edd5f4cbc3b321cf23492ebea2dc0b4f0
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-06-18 23:01:26 +0700

    add boolean to check quarter.

--- BasketballScoutAndroidProject/BasketballScount/app/src/main/java/porcomsci/basketballscout/com/basketballscount/utility/SegueHelper.java
+++ BasketballScoutAndroidProject/BasketballScount/app/src/main/java/porcomsci/basketballscout/com/basketballscount/utility/SegueHelper.java
@@ -8,5 +8,10 @@ public class SegueHelper {
     public static boolean team1Chosen = false;
     public static boolean team2Chosen = false;
     public static int playerChoosingSequence;
+    public static boolean quarter1IsPlayed = false;
+    public static boolean quarter2IsPlayed = false;
+    public static boolean quarter3IsPlayed = false;
+    public static boolean quarter4IsPlayed = false;
+    public static boolean quarter5IsPlayed = false;
 
 }
